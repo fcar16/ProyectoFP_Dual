a15d128a4a0f3d86a4dc6403e51aa1f8a39d3f50
antonio.sillero <antoniosillero@ceitartessos.com>
2025-02-11 13:35:54 +0100

    Resource apicompany

--- Solicitud_Alumno/app/Http/Controllers/API/ApiCompanyController.php
+++ Solicitud_Alumno/app/Http/Controllers/API/ApiCompanyController.php
@@ -6,18 +6,18 @@ use Illuminate\Http\Request;
 use App\Models\Company;
 use Illuminate\Http\JsonResponse;
 use App\Http\Requests\ApiCompanyRequest;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\ApiCompanyResource;
 
 class ApiCompanyController
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): JsonResponse
+    public function index(): JsonResource
     {
-        // Recuperamos todos los elementos
-        $companies = Company::all();
-        // Enviamos esos datos mediante un formato de API RESTFUL con JSON
-        return response()->json($companies, 200);
+       
+        return ApiCompanyResource::collection(Company::all());
     }
 
     /**
@@ -25,44 +25,43 @@ class ApiCompanyController
      */
     public function store(ApiCompanyRequest $request): JsonResponse
     {
-        Company::create($request->all());
+        $company = Company::create($request->all());
         return response()->json([
-            'success' => true
-        ], 201); // El estado 201 corresponde a la creación correcta de un nuevo elemento
+            'success' => true,
+            'data' => new ApiCompanyResource($company)
+        ], 201); // El estado 201 corresponde a una creación correcta
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(int $id): JsonResponse
+    public function show(int $id): JsonResource
     {
         // Busca un elemento con su ID y lo devuelve
         $company = Company::find($id);
         if ($company) {
-            return response()->json($company, 200);
+            return new ApiCompanyResource($company);
         } else {
-            return response()->json(['error' => 'Company not found'], 404);
+            return new JsonResource(['error' => 'Company not found'], 404);
         }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(ApiCompanyRequest $request, int $id): JsonResponse
-    {
-        $company = Company::find($id);
-        if ($company) {
-            $company->name = $request->name;
-            $company->website = $request->website;
-            $company->NIF = $request->NIF;
-            $company->save(); // Guardamos los cambios
-            return response()->json([
-                'success' => true
-            ], 200); // El estado 200 corresponde a una actualización correcta
-        } else {
-            return response()->json(['error' => 'Company not found'], 404);
-        }
+    public function update(ApiCompanyRequest $request, int $id): JsonResource
+{
+    $company = Company::find($id);
+    if ($company) {
+        $company->name = $request->name;
+        $company->website = $request->website;
+        $company->NIF = $request->NIF;
+        $company->save(); // Guardamos los cambios
+        return new ApiCompanyResource($company);
+    } else {
+        return response()->json(['error' => 'Company not found'], 404);
     }
+}
 
     /**
      * Remove the specified resource from storage.
--- Solicitud_Alumno/app/Models/Company.php
+++ Solicitud_Alumno/app/Models/Company.php
@@ -2,15 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Company extends Model
 {
     protected $fillable = ['name', 'website', 'NIF'];
+    
     use HasFactory;
-
+    protected $hidden =['created_at', 'updated_at'];
     public function students(): BelongsToMany
     {
         return $this->belongsToMany(Student::class)->withPivot('question');
