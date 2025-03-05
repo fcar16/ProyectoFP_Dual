6720d396fb74c0f2271748538e1ac50aa4f27d95
rafaTemp <rafaluva03@gmail.com>
2025-03-05 14:42:07 +0100

    error cors

--- Solicitud_Alumno/config/cors.php
+++ Solicitud_Alumno/config/cors.php
@@ -7,7 +7,7 @@ return [
 
     //'allowed_origins' => ['http://localhost:5173'], // Reemplaza con la URL de tu frontend
 
-    'allowed_origins' => ['http://docvalle.duckdns.org:80'], // Reemplaza con la URL de tu frontend
+    'allowed_origins' => ['http://docvalle.duckdns.org'], // Reemplaza con la URL de tu frontend
 
     'allowed_origins_patterns' => [],
 
