bc00cfca1ad579b6af0cfdac77b1ff8dc54cdd3a
rafaTemp <rafaluva03@gmail.com>
2025-03-05 12:38:51 +0100

    dockerizando

--- Solicitud_Alumno/config/cors.php
+++ Solicitud_Alumno/config/cors.php
@@ -5,7 +5,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'], // Reemplaza con la URL de tu frontend
+    //'allowed_origins' => ['http://localhost:5173'], // Reemplaza con la URL de tu frontend
+
+    'allowed_origins' => ['http://rlucval.duckdns.org:80'], // Reemplaza con la URL de tu frontend
 
     'allowed_origins_patterns' => [],
 
